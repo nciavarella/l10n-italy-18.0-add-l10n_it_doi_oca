terradelsole/l10n-italy

[FIX] ricevute bancarie ASCII instead UTF-8

l10n_it_ricevute_bancarie/wizard/wizard_riba_file_export.py
@@ -396,7 +396,7 @@ class RibaFileExport(models.TransientModel):
             arrayRiba.append(Riba)
 
         out = base64.encodebytes(
-            self._creaFile(array_testata, arrayRiba).encode("utf8")
+            self._creaFile(array_testata, arrayRiba).encode("ascii", errors="replace")
         )
         self.write(
             {"state": "get", "riba_txt": out, "file_name": "%s.txt" % order_obj.name}

nciavarella/l10n-italy-18.0-add-l10n_it_doi_oca

l10n_it_riba/wizard/wizard_riba_file_export.py
@@ -398,7 +398,7 @@ class RibaFileExport(models.TransientModel):
             array_riba.append(riba)
 
         out = base64.encodebytes(
-            self._createFile(array_header, array_riba).encode("utf8")
+            self._createFile(array_header, array_riba).encode("ascii", errors="replace")
         )
         self.write(
             {"state": "get", "riba_txt": out, "file_name": "%s.txt" % order_obj.name}